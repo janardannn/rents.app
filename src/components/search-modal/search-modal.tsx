2b8f69cc0081874e3f5229265707c9e8975bb378
janardannn <janardonhazarika7@gmail.com>
2025-08-17 22:40:43 +0530

    prototype image updated

--- src/components/search-modal/search-modal.tsx
+++ src/components/search-modal/search-modal.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const SearchModal = () => {
+    const [visibility, setVisibility] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setVisibility(!visibility);
+        console.log("Search modal clicked");
+    }
     return (
-        <div className="search-modal mt-4 bg-white rounded-xl shadow-xl p-6 max-w-xl w-full pointer-events-auto cursor-pointer text-xl text-gray-500 border-none">
-            Discover best properties near you matching your needs
-        </div>
+        <>
+            <div
+                onClick={handleClick}
+                className="search-modal mt-4 bg-white rounded-xl shadow-xl p-6 max-w-xl w-full pointer-events-auto cursor-pointer text-xl text-gray-500 border-none">
+                Discover best properties near you matching your checklist
+            </div>
+        </>
     )
 }
 
